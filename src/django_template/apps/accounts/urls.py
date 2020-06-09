eec1bed3357956b50ef306550c96fbef88de8190
Emily <12156026+emilyjt@users.noreply.github.com>
2020-06-09 23:12:20 +0100

    feat: adding very basic templates, static, views and urls

---
+++ src/django_template/apps/accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+
+# https://docs.djangoproject.com/en/dev/topics/auth/default/#module-django.contrib.auth.views
+
+app_name = "accounts"
+urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
+]
+
+# This is a list of the included urls
+# accounts/login/ [name='login']
+# accounts/logout/ [name='logout']
+# accounts/password_change/ [name='password_change']
+# accounts/password_change/done/ [name='password_change_done']
+# accounts/password_reset/ [name='password_reset']
+# accounts/password_reset/done/ [name='password_reset_done']
+# accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/reset/done/ [name='password_reset_complete']
--- src/django_template/apps/main/views.py
+++ src/django_template/apps/main/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+import random
+
+
+# https://docs.djangoproject.com/en/3.0/ref/class-based-views/base/
+class HomePageView(TemplateView):
+    template_name = "main/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["number"] = random.randrange(1, 100)
+        return context
