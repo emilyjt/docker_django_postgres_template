2a5da8ccc70daa029509dbf06d4ed228b7d415bd
Emily <12156026+emilyjt@users.noreply.github.com>
2020-06-09 23:43:46 +0100

    test: adding some basic tests to the home view

--- src/django_template/apps/main/tests.py
+++ src/django_template/apps/main/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse, resolve
+
+from .views import HomePageView
+
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse("main:home")
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve("/")
+        self.assertEquals(view.func.__name__, HomePageView.as_view().__name__)
