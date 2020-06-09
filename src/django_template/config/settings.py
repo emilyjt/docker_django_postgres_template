6a50d284824e556859370acf2fb1ff9db225823a
Emily <12156026+emilyjt@users.noreply.github.com>
2020-06-09 22:26:57 +0100

    refactor: settings.py inspired by cookiecutter-django

--- src/django_template/config/settings.py
+++ src/django_template/config/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
+
+https://github.com/pydanny/cookiecutter-django/blob/master/%7B%7Bcookiecutter.project_slug%7D%7D/config/settings/base.py
 """
 
 import os
@@ -15,106 +17,166 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^9d63l(*x9k4gwg@*bvxt^1d97&xo(bw!z8ox#tn^zu-$yuf!n'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+# General
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/3.0/ref/settings/#debug
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Local time zone. Choices are
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# though not all of them may be available with every OS.
+# In Windows, this must be set to your system time zone.
+TIME_ZONE = "GB"
 
+# https://docs.djangoproject.com/en/3.0/ref/settings/#language-code
+LANGUAGE_CODE = "en-gb"
 
-# Application definition
+# https://docs.djangoproject.com/en/3.0/ref/settings/#site-id
+SITE_ID = 1
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+# https://docs.djangoproject.com/en/3.0/ref/settings/#use-i18n
+USE_I18N = True
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#use-l10n
+USE_L10N = True
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#use-tz
+USE_TZ = True
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#locale-paths
+# LOCALE_PATHS = os.path.join(BASE_DIR, "locale")
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#secret-key
+SECRET_KEY = "^9d63l(*x9k4gwg@*bvxt^1d97&xo(bw!z8ox#tn^zu-$yuf!n"
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+# Database
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
+# Urls
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/3.0/ref/settings/#root-urlconf
+ROOT_URLCONF = "config.urls"
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#wsgi-application
+WSGI_APPLICATION = "config.wsgi.application"
+
+# Apps
+# ------------------------------------------------------------------------------
+
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
+THIRD_PARTY_APPS = []
+
+LOCAL_APPS = ["apps.main", "apps.accounts"]
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#installed-apps
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# Migrations
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/3.0/ref/settings/#migration-modules
+
+
+# Authentication
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/3.0/ref/settings/#authentication-backends
+# AUTHENTICATION_BACKENDS = [
+#     "django.contrib.auth.backends.ModelBackend",
+#     "allauth.account.auth_backends.AuthenticationBackend",
+# ]
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-user-model
+AUTH_USER_MODEL = "accounts.User"
+# https://docs.djangoproject.com/en/3.0/ref/settings/#login-redirect-url
+LOGIN_REDIRECT_URL = "accounts:redirect"
+# https://docs.djangoproject.com/en/3.0/ref/settings/#login-url
+LOGIN_URL = "accounts:login"
+
+# Passwords
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+]
+
+# Middleware
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/3.0/ref/settings/#middleware
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'config.urls'
+# Static
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/3.0/ref/settings/#static-root
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+# https://docs.djangoproject.com/en/3.0/ref/settings/#static-url
+STATIC_URL = "/static/"
+
+# https://docs.djangoproject.com/en/3.0/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+
+# Templates
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/3.0/ref/settings/#templates
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            # https://docs.djangoproject.com/en/3.0/ref/settings/#template-loaders
+            # https://docs.djangoproject.com/en/3.0/ref/templates/api/#loader-types
+            # "loaders": [
+            #     "django.template.loaders.filesystem.Loader",
+            #     "django.template.loaders.app_directories.Loader",
+            # ],
+            # https://docs.djangoproject.com/en/3.0/ref/settings/#template-context-processors
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+# Admin
+# ------------------------------------------------------------------------------
+# Django Admin URL.
+ADMIN_URL = "admin/"
 
+# https://docs.djangoproject.com/en/3.0/ref/settings/#admins
+ADMINS = [("Emily", "***REMOVED***")]
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
+# https://docs.djangoproject.com/en/3.0/ref/settings/#managers
+MANAGERS = ADMINS
