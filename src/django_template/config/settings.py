c3b1ca9593fab28ee90b61e02b931bde44336d12
Emily <12156026+emilyjt@users.noreply.github.com>
2020-06-09 22:50:41 +0100

    fix: fixing list error for 'STATICFILES_DIRS'

--- src/django_template/config/settings.py
+++ src/django_template/config/settings.py
@@ -142,7 +142,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 # https://docs.djangoproject.com/en/3.0/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Templates
 # ------------------------------------------------------------------------------
