c675fe252b84f75c5050dabc0b1173657ed05aa8
Emily <12156026+emilyjt@users.noreply.github.com>
2020-06-10 10:55:57 +0100

    docs: correcting a link, and adding an example POST

--- src/django_template/apps/main/views.py
+++ src/django_template/apps/main/views.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 import random
 
 
-# https://docs.djangoproject.com/en/3.0/ref/class-based-views/base/
+# https://docs.djangoproject.com/en/3.0/ref/class-based-views/
 class HomePageView(TemplateView):
     template_name = "main/home.html"
 
@@ -11,3 +11,26 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["number"] = random.randrange(1, 100)
         return context
+
+
+# Taken from https://docs.djangoproject.com/en/3.0/topics/class-based-views/mixins/#using-singleobjectmixin-with-view
+
+# from django.http import HttpResponseForbidden, HttpResponseRedirect
+# from django.urls import reverse
+# from django.views import View
+# from django.views.generic.detail import SingleObjectMixin
+# from books.models import Author
+
+# class RecordInterest(SingleObjectMixin, View):
+#     """Records the current user's interest in an author."""
+#     model = Author
+
+#     def post(self, request, *args, **kwargs):
+#         if not request.user.is_authenticated:
+#             return HttpResponseForbidden()
+
+#         # Look up the author we're interested in.
+#         self.object = self.get_object()
+#         # Actually record interest somehow here!
+
+#         return HttpResponseRedirect(reverse('author-detail', kwargs={'pk': self.object.pk}))
