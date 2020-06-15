a17607c37afd45bb9d2153ae6e09db294828a70a
Emily <12156026+emilyjt@users.noreply.github.com>
2020-06-15 23:44:10 +0100

    feat: including a custom, optional, "email based login" user model

--- src/django_template/apps/accounts/admin.py
+++ src/django_template/apps/accounts/admin.py
@@ -4,3 +4,54 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 admin.site.register(User, UserAdmin)
+
+""" <--- delete if necessary
+'''
+The code here is a counterpart to the custom user model
+'''
+
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
+# from django.contrib.auth import get_user_model
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = ("email", "username", "first_name", "last_name", "is_staff")
+
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
+
+    readonly_fields = ("last_login", "date_joined")
+
+    ordering = ("email",)
+
+"""  # <--- delete if necessary
