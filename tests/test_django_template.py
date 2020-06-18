7970025694815b7253463aca3fe57f8f8b280526
Emily <12156026+emilyjt@users.noreply.github.com>
2020-06-18 15:51:25 +0100

    test: changing the version test so it actually works

--- tests/test_django_template.py
+++ tests/test_django_template.py
@@ -1,5 +1,8 @@
+import unittest
+
 from django_template import __version__
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+class TestDjangoTemplate(unittest.TestCase):
+    def test_version(self):
+        self.assertEqual(__version__, "0.1.0")
