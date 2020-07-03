7180be23f7a225ec5e29c016042e7b4af88b9da5
Emily <12156026+emilyjt@users.noreply.github.com>
2020-07-03 21:13:30 +0100

    feat: adding an example verbose_name to apps.main

--- src/django_template/apps/main/apps.py
+++ src/django_template/apps/main/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class MainConfig(AppConfig):
     name = "apps.main"
+    verbose_name = "Main"  # change this to anything and the django admin will reflect what is here.
