635218cd39d68ac44af9d0a4433c0172d639fb7b
Emily <12156026+emilyjt@users.noreply.github.com>
2020-07-30 23:55:00 +0100

    working docker for development and production

---
+++ docker/production/entrypoint.production.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$POSTGRES_HOST" = "postgresdb" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py collectstatic --noinput
+
+exec "$@"
