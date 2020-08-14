83126b26213eb03a99e72a03ed909d024390e5f8
Emily <12156026+emilyjt@users.noreply.github.com>
2020-08-14 21:22:32 +0100

    adding some comments

--- src/django_template/django_template/main/views.py
+++ src/django_template/django_template/main/views.py
@@ -1,10 +1,23 @@
-# We need random to demonstrate passing values to a webpage
-import random
+"""
+# https://spookylukey.github.io/django-views-the-right-way/the-pattern.html
+# spookylukey is a core @django developer, freelancer.
+# https://github.com/spookylukey
+#
+# Below is his recommendation on views:
+
+from django.template.response import TemplateResponse
+
+def example_view(request, arg):
+    return TemplateResponse(request, 'example.html', {})
+
+"""
+
+import random  # We need random to demonstrate passing values to a webpage
 
 from django.template.response import TemplateResponse
 
 
-def home(request, arg=None):
+def home(request):
     return TemplateResponse(
         request, "main/home.html", {"number": random.randrange(1, 100)}
     )
