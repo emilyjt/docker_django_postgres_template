b5f8ce33004d1f337ac5cd599b24d22d4faa8b22
Emily <12156026+emilyjt@users.noreply.github.com>
2020-11-27 23:14:29 +0000

    `main` to `core` - leftover change from 8404b90a

--- src/django_template/django_template/core/apps.py
+++ src/django_template/django_template/core/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = "django_template.core"
-    verbose_name = "Main"  # change this to anything and the django admin will reflect what is here.
+    verbose_name = "Core"  # change this to anything and the django admin will reflect what is here.
