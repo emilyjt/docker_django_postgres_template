3691f996d40fc0bd7fdd2bda971b1463569c592a
Emily <12156026+emilyjt@users.noreply.github.com>
2020-11-30 15:35:25 +0000

    integrate automatic .env generator

--- scripts/start_project.py
+++ scripts/start_project.py
@@ -1,4 +1,6 @@
 import os
+import string
+import secrets
 
 
 def _slugify(text):
@@ -41,10 +43,28 @@ if __name__ == "__main__":
     project_name = input("What would you like your project to be called?\n")
     slug = _slugify(project_name)
 
-    # Delete everything reachable from the directory named in "top",
-    # assuming there are no symbolic links.
-    # CAUTION:  This is dangerous!  For example, if top == '/', it
-    # could delete all your disk files.
+    ##########
+    # Generate the .env secrets
+    ##########
+    alphabet = string.ascii_letters + string.digits
+
+    for _root, _dirs, _files in os.walk(os.path.join(BASE_DIR, ".envs"), topdown=False):
+        password = "".join(secrets.choice(alphabet) for i in range(32))
+
+        for _file in _files:
+            with open(os.path.join(_root, _file)) as open_file:
+                file_data = open_file.read()
+
+            file_data = file_data.replace("template_default_secret", password)
+
+            with open(os.path.join(_root, _file), "w") as open_file:
+                open_file.write(file_data)
+
+    ##########
+    # Rename files and folders
+    ##########
+    files_total = 0
+    dirs_total = 0
 
     for _root, _dirs, _files in os.walk(BASE_DIR, topdown=False):
         if ".git" in _root:
@@ -72,28 +92,21 @@ if __name__ == "__main__":
             with open(os.path.join(_root, _file)) as open_file:
                 file_data = open_file.read()
 
-            file_data = file_data.replace("django_template", slug)
+            new_data = file_data.replace("django_template", slug)
 
-            with open(os.path.join(_root, _file), "w") as open_file:
-                open_file.write(file_data)
+            if new_data != file_data:
+                with open(os.path.join(_root, _file), "w") as open_file:
+                    open_file.write(file_data)
+
+                    files_total += 1
 
         for _dir in _dirs:
-            print(
-                f"Renaming: {os.path.join(_root, _dir)} to: {os.path.join(_root, _dir.replace('django_template', slug))}"
-            )
-            os.rename(
-                os.path.join(_root, _dir),
-                os.path.join(_root, _dir.replace("django_template", slug)),
-            )
+            if "django_template" in _dir:
+                os.rename(
+                    os.path.join(_root, _dir),
+                    os.path.join(_root, _dir.replace("django_template", slug)),
+                )
 
-    # try:
-    #     os.rename(
-    #         os.path.join(BASE_DIR, "src", "django_template", "django_template"),
-    #         os.path.join(BASE_DIR, "src", "django_template", slug),
-    #     )
-    #     os.rename(
-    #         os.path.join(BASE_DIR, "src", "django_template"),
-    #         os.path.join(BASE_DIR, "src", slug),
-    #     )
-    # except FileNotFoundError:
-    #     pass
+                dirs_total += 1
+
+        print(f"{ files_total } files have been updated, and { dirs_total } directories have been renamed.")
