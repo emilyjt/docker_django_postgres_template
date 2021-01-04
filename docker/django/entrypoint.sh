a6875fc9917daed11ef1f21fd3e6948ad69df2bc
Emily <12156026+emilyjt@users.noreply.github.com>
2021-01-04 16:49:18 +0000

    move migrate/collect static to docker-compose

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-# Check if the database has started
-# We could also use ${POSTGRES_PORT:-5432} to provide a default value
+## Check if the database has started
+## We could also use ${POSTGRES_PORT:-5432} to provide a default value
 if [ -n "$POSTGRES_HOST" ] && [ -n "$POSTGRES_PORT" ]
 then
     /usr/local/bin/wait-for-it.sh "$POSTGRES_HOST:$POSTGRES_PORT" --strict --timeout=300
 fi
 
-python manage.py makemigrations
-python manage.py migrate
+## The below code has been migrated into the docker-compose files.
 
-# We do not want to run collectstatic during development, as we are probably
-# using the django dev server
-if [ "$DJANGO_SETTINGS_MODULE" != "config.settings.development" ]
-then
-    python manage.py collectstatic --noinput
-fi
+# python manage.py makemigrations
+# python manage.py migrate
+
+## We do not want to run collectstatic during development, as we are probably
+## using the django dev server
+# if [ "$DJANGO_SETTINGS_MODULE" != "config.settings.development" ]
+# then
+#     python manage.py collectstatic --noinput
+# fi
 
 exec "$@"
